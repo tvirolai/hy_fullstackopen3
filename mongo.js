0b91269a570844e2c6d7d241d2b3b5307c8d97ed
Tuomo Virolainen <tuomo.virolainen@siili.com>
2018-06-23 15:38:54 +0300

    3.12

---
+++ mongo.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const url = `mongodb://${process.env.MONGOUSER}:${process.env.MONGOPASS}@ds161700.mlab.com:61700/hy_fullstack`
+
+mongoose.connect(url)
+
+const name = process.argv[2]
+const number = process.argv[3]
+
+console.log(`Lisätään henkilö ${name} numero ${number} luetteloon.`)
+
+const Person = mongoose.model('Person', {
+  name: String,
+  number: String
+})
+
+const person = new Person({
+  name: name,
+  number: number
+})
+
+person
+  .save()
+  .then(response => {
+    console.log('note saved!')
+    mongoose.connection.close()
+  })
